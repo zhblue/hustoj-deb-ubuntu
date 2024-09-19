3dca8c8ce885a3ad7445116647d7940b0c668a2f
张浩斌 <newsclan@gmail.com>
2024-09-19 21:54:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ set -ex \
 && git clone https://github.com/zhblue/hustoj-deb-ubuntu.git \
 && mv hustoj/trunk/* hustoj-deb-ubuntu \
 && cd hustoj-deb-ubuntu && dpkg-buildpackage 
-
-
+cd ..
+sudo dpkg -i *.deb || sudo apt-get install -f -y
+exit 0
