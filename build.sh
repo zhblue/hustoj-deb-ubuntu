422192df1c80b2e186d2c6e283f5576d8719b1b6
张浩斌 <newsclan@gmail.com>
2024-09-19 20:11:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@
 sudo apt-get install debhelper
 set -ex \
 && git clone https://github.com/zhblue/hustoj.git \
-&& cd hustoj \
-&& cd .. \
 && git clone https://github.com/zhblue/hustoj-deb-ubuntu.git \
 && mv hustoj/trunk/* hustoj-deb-ubuntu \
 && cd hustoj-deb-ubuntu && dpkg-buildpackage 
