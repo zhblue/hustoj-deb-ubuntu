27f627abcaca64fb45fc72b8ae8424f5c96fdf6e
张浩斌 <newsclan@gmail.com>
2024-09-19 22:25:39 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,5 +14,16 @@ set -ex \
 && mv hustoj/trunk/* hustoj-deb-ubuntu \
 && cd hustoj-deb-ubuntu && dpkg-buildpackage 
 cd ..
+PHP_VER=`apt-cache search php-fpm|grep -e '[[:digit:]]\.[[:digit:]]' -o`
+if [ "$PHP_VER" = "" ] ; then PHP_VER="8.1"; fi
+for pkg in net-tools make g++ php$PHP_VER-fpm nginx php$PHP_VER-mysql php$PHP_VER-common php$PHP_VER-gd php$PHP_VER-zip php$PHP_VER-mbstring php$PHP_VER-xml php$PHP_VER-curl php$PHP_VER-intl php$PHP_VER-xmlrpc php$PHP_VER-soap php-yaml php-apcu tzdata
+do
+        while ! sudo apt-get install -y "$pkg"
+        do
+                sudo dpkg --configure -a
+                sudo apt-get install -f
+                echo "Network fail, retry... you might want to change another apt source for install"
+        done
+done
 sudo dpkg -i *.deb || sudo apt-get install -f -y
 exit 0
